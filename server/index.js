e756b3f95ab523e04ec4638d793bdd3d81446b2f
MJB <matt.berti13@gmail.com>
2022-08-08 17:37:51 -0500

    established server connection with socket.io middleware

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const { createServer } = require('http');
 const app = express();
 const { Server } = require('socket.io');
+const cors = require('cors');
+
+//middleware for cors 
+app.use(cors());
+
+
 
 const httpServer = createServer();
-
 const io = new Server(httpServer, {
     //TODO: check cors options for socket.io
     cors:{
@@ -17,4 +22,6 @@ io.on('connection', (socket) =>{
     // ...
 });
 
-httpServer.listen(3000);
+httpServer.listen(3000, () =>{
+    console.log("Connected to server on Port 3000")
+});
